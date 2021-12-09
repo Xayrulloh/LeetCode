735f2a626cdf856d245cbdc8647f01ee2b10eb99
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2021-12-09 21:29:04 +0500

    Easy exercises

---
+++ LeetCodes.js
@@ -0,0 +1,95 @@
+// 1 exercise
+/*var twoSum = function(arr, target) {
+    let newarr = []
+    for (let a = 0; a < arr.length; a++) {
+        for (let b = 0; b < arr.length; b++) {
+            if (a != b) {
+                if (arr[a] + arr[b] === target) {
+                    newarr.push(a)
+                    newarr.push(b)
+                    return newarr
+                }
+            }
+        }
+    }
+};
+console.log(twoSum([3, 2, 4], 6));*/
+
+// 2 exercise
+/*let palindrome = (num) => {let rev = Number(String(num).split('').reverse().join('')); return rev === num ? true : false}
+console.log(palindrome(121));*/
+
+// 3 exercise
+/*function romanToInt(str) {
+    let sum = 0
+    for (let a = str.length-1; a >= 0; a--) {
+        if (str[a - 1]=== 'X' && str[a] === 'L') {
+            sum += 40
+            a -= 1
+            continue
+        }
+        if (str[a - 1]=== 'C' && str[a] === 'D') {
+            sum += 400
+            a -= 1
+            continue
+        }
+        if (str[a - 1]=== 'X' && str[a] === 'C') {
+            sum += 90
+            a -= 1
+            continue
+        }
+        if (str[a - 1]=== 'C' && str[a] === 'M') {
+            sum += 900
+            a -= 1
+            continue
+        }
+        if (str[a - 1]=== 'I' && str[a] === 'X') {
+            sum += 9
+            a -= 1
+            continue
+        }
+        if (str[a - 1]=== 'I' && str[a] === 'V') {
+            sum += 4
+            a -= 1
+            continue
+        }
+        if (str[a] === 'I') sum += 1
+        if (str[a] === 'V') sum += 5
+        if (str[a] === 'X') sum += 10
+        if (str[a] === 'L') sum += 50
+        if (str[a] === 'C') sum += 100
+        if (str[a] === 'D') sum += 500
+        if (str[a] === 'M') sum += 1000
+    }
+    return sum
+}
+console.log(romanToInt("MCD"));*/
+
+// 4 exercise
+/*function longestCommonPrefix(arr) {
+    let str = ''
+    let solishtirish2 = arr[0]
+    let sum = 0
+    for (let a = 0; a < arr[0].length; a++) {
+        for (let b = 0; b < arr.length; b++) {
+            if (solishtirish2[a] === arr[b][a]) {
+                sum++
+            }
+            if (solishtirish2[a] != arr[b][a]) return str
+        }
+        if (sum == arr.length) str += solishtirish2[a]
+        sum = 0
+    }
+    return str
+}
+console.log(longestCommonPrefix(["cir","car"]));*/
+
+
+
+
+
+
+
+
+
+
