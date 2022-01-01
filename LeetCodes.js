6ea2c11451fa355de727e6ab6a09407bddde3b03
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-01-01 16:22:40 +0500

    new leetcode exercise is completed

--- LeetCodes.js
+++ LeetCodes.js
@@ -1872,6 +1872,27 @@ console.log(maxNumberOfBalloons("nlaebolko"));*/
 console.log(uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]));*/
 
 // Split a String in Balanced Strings
+/*function balancedStringSplit(str) {
+    let res = 0, balance = 0
+    for (const a of str) {
+        if (a === 'R') balance++
+        else balance--
+        if (balance === 0) res++
+    }
+    return res
+}
+console.log(balancedStringSplit("RLLLLRRRLR"));*/
+
+// Check If It Is a Straight Line
+/*function checkStraightLine(arr) {
+    if (arr.length === 2) return true
+    for (let a = 0; a < arr.length - 2; a++) {
+        if ((arr[a + 2][1] - arr[a + 1][1]) * (arr[a + 1][0] - arr[a][0] != (arr[a + 1][1] - arr[a][1]) * (arr[a + 2][0] - arr[a + 1][0]))) return false
+    }
+    return true
+}
+console.log(checkStraightLine([[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]));*/
+
 
 
 
